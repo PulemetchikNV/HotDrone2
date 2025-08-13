813becde647e4efd9cd990756ca9ee8e21ae0cfe
pulemet4iknv <pulemetik.ai@gmail.com>
2025-08-13 16:46:57 +0300

    upd drones

--- drone/const.py
+++ drone/const.py
@@ -1,11 +1,3 @@
-DRONE_LIST = 'drone6,drone7,drone19'
-LEADER_DRONE = 'drone7'
+DRONE_LIST = 'drone15,drone16,drone17'
+LEADER_DRONE = 'drone15'
 DRONES_TOTAL=3
-
-# Явные роли дронов для подбора по рецепту (используется лидером)
-# Возможные значения: 'D', 'S', 'ANY'
-DRONE_ROLES = {
-    'drone6': 'D',
-    'drone7': 'D',
-    'drone19': 'S',
-}
